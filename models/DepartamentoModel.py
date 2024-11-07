fa15785dcbec43217bd81e0fb9af4808ad92716f
GwynbleiddOfR <se.apileo@duocuc.cl>
2024-11-06 21:34:45 -0300

    first commit

---
+++ models/DepartamentoModel.py
@@ -0,0 +1,11 @@
+from app import db
+
+class Departamento(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    numero = db.Column(db.String(10), unique=True, nullable=False)
+    
+    def serialize(self):
+        return {
+            'id': self.id, 
+            'numero': self.numero
+        }
